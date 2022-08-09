b29a9208b17092248e4298d38a324f19936912be
Kanu Rowland <Kanurowland92@gmail.com>
2022-08-09 23:25:47 +0100

    updated toggle functionality from using jQuery to using Vanilla Javascript

---
+++ index.js
@@ -0,0 +1,25 @@
+/*
+Using jQuery to add toggle functionality
+
+$(document).ready(function(){
+    $('.hamburger-button').click(function(){
+        $('.sidebar').toggleClass('active');
+    }),
+    $('.hamburger-button').click(function(){
+        $('.body').toggleClass('activeBody')
+
+    })
+});
+
+*/
+
+
+// Using Vanilla Javascript 
+document.querySelector(".hamburger-button").addEventListener("click", switchProperty);
+function switchProperty() {
+    let sidebarToggle = document.querySelector(".sidebar");
+    let bodyToggle = document.querySelector("body");
+    sidebarToggle.classList.toggle('active');
+    bodyToggle.classList.toggle('activeBody')
+
+}
