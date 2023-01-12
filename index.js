3cf34bfd7675e35084bf58a3719fc04175107cc3
Kanu Rowland <Kanurowland92@gmail.com>
2023-01-13 00:35:06 +0100

    added light/dark mode switch

--- index.js
+++ index.js
@@ -33,3 +33,13 @@ document.querySelector(".hamburger-button").addEventListener("click", () => {
     sidebarToggle.classList.toggle('active');
     bodyToggle.classList.toggle('activeBody')
 });
+
+var icon = document.getElementById("moon-icon")
+icon.onclick = function() {
+    document.body.classList.toggle("dark-theme")
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "icons/sun.png";
+    }else{
+        icon.src = "icons/moon.png"
+    }
+}
